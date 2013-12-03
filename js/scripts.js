a2afe60d29733e29549bb2271e4cf00a6c39e8ab
Martynas <martynas.buivys@gmail.com>
2013-12-03 10:01:05 +0000

    Merged my front-end files with the new file structure

--- js/scripts.js
+++ js/scripts.js
@@ -30,9 +30,27 @@ $(document).ready(function() {
     });
 
     // TODO: Convert multiple select dropdown to dropdown checklist
-
+    $("#region").select2();
+    $("#country").select2();
     // TODO: implement reset function
     $( "#reset").click(function() {
-        // Implement me
+        alert("yashi akasa");
     });
+
+    // Load CSV contents
+    var data = [
+                  ["v1","v2","v3"],
+                  ["v11","v22","v33"]
+               ];
+
+    for (var i = 0; i < data.length; i++) {
+        var value = "<tr>";
+        for (var j = 0; j <= data.length; j++)
+            value += "<td>" + data[i][j] + "</td>";
+        value += "</tr>";
+        $("#data-table tr:last").after(value);
+    }
+
+
+
 });
