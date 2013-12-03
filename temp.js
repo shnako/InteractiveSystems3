8ffde4dc58cabd01fe3f16e7a8411e89d4e6273c
rrobi <robert.szkokan@gmail.com>
2013-12-03 14:52:41 +0000

    regions

---
+++ temp.js
@@ -0,0 +1,7 @@
+var Europe = ["Albania"];
+var Asia = ["Afghanistan"];
+var Africa = ["Algeria"];
+var North_America = [""];
+var South_America = [""];
+var Central_America = [""];
+var Australia = [""];
